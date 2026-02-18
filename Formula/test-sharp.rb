49bcc4d538744bf7fb75c0f38aac02259681cbbe
Kevin Koslowski <kevin@madwizard.de>
2026-02-18 16:38:48 +0000

    test-sharp: update to v1.3.0

--- Formula/test-sharp.rb
+++ Formula/test-sharp.rb
@@ -1,8 +1,8 @@
 class TestSharp < Formula
   desc "Hello World .NET sample"
   homepage "https://github.com/mad0x20wizard/test-sharp"
-  url "https://github.com/mad0x20wizard/test-sharp/archive/refs/tags/v1.2.1.tar.gz"
-  sha256 "d489c018bde6a32f54fc07f53c6f2072d3d869218b42790a1236d0a9c7dd36c4"
+  url "https://github.com/mad0x20wizard/test-sharp/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "358c56ae9fb7e90a4adb29458f25721d158b4a7587b35e4ea9d01df6b46c1cfe"
   license "MIT"
 
   bottle do
