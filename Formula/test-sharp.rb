f0fa1ea568c99f57d772102df4480ba768ffa175
Kevin Koslowski <kevin@madwizard.de>
2026-02-18 15:21:44 +0000

    test-sharp 1.2.4

--- Formula/test-sharp.rb
+++ Formula/test-sharp.rb
@@ -1,8 +1,8 @@
 class TestSharp < Formula
   desc "Hello World .NET sample"
   homepage "https://github.com/mad0x20wizard/test-sharp"
-  url "https://github.com/mad0x20wizard/test-sharp/archive/refs/tags/v1.2.1.tar.gz"
-  sha256 "d489c018bde6a32f54fc07f53c6f2072d3d869218b42790a1236d0a9c7dd36c4"
+  url "https://github.com/mad0x20wizard/test-sharp/archive/refs/tags/v1.2.4.tar.gz"
+  sha256 "aaf69178836e1588a402330867d4fc4af5e9eedab5dc1401de0bee952b05971b"
   license "MIT"
 
   bottle do
