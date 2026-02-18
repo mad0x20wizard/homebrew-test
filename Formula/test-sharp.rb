3c125800be38b0d5c7f357e6102caac0d4d1e981
Kevin Koslowski <kevin@madwizard.de>
2026-02-18 17:47:12 +0100

    explicit version

--- Formula/test-sharp.rb
+++ Formula/test-sharp.rb
@@ -12,8 +12,8 @@ class TestSharp < Formula
 
   depends_on "dotnet" => [:build]
   depends_on "brotli"
-  depends_on "icu4c"
-  depends_on "openssl"
+  depends_on "icu4c@78"
+  depends_on "openssl@3"
 
   def install
     dotnet_info = Utils.safe_popen_read("dotnet", "--info")
