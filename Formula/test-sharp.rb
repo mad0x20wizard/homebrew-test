cc5ec60ec152b95e5bcbb88bf0542d8cf5e03cd2
Kevin Koslowski <kevin@madwizard.de>
2026-02-19 10:28:47 +0100

    test

--- Formula/test-sharp.rb
+++ Formula/test-sharp.rb
@@ -3,12 +3,7 @@ class TestSharp < Formula
   homepage "https://github.com/mad0x20wizard/test-sharp"
   url "https://github.com/mad0x20wizard/test-sharp/archive/refs/tags/v3.1.tar.gz"
   sha256 "6bb607c861931097fbbb8e617ca46440850747460f86b74c568039450ed6fabd"
-  license "MIT"
-
-  bottle do
-    root_url "https://ghcr.io/v2/mad0x20wizard/test"
-    sha256 cellar: :any_skip_relocation, arm64_linux: "40301f4bb9d70d730d367080db6c023995404e0a1b7a8bf5aef0f5937bd53471"
-  end
+  license "GPL-3.0-or-later"
 
   depends_on "dotnet" => [:build]
   depends_on "brotli"
@@ -21,24 +16,13 @@ class TestSharp < Formula
   end
 
   def install
-    dotnet_info = Utils.safe_popen_read("dotnet", "--info")
-
-    rid_line = dotnet_info.lines.find do |l|
-        l.start_with?(" RID:", "RID:")
-    end
-
-    odie "Could not determine .NET RID from `dotnet --info`" if rid_line.nil?
-
-    rid = rid_line.split(":", 2).last&.strip
-    odie "Could not parse RID from `dotnet --info`" if rid.blank?
-
-    # Adjust path if your csproj lives elsewhere
     system "dotnet", "publish", "test-sharp.csproj",
             "-c", "Release",
-            "-r", rid,
-            "--self-contained", "true",
+            "-r", determine_rid,
+            "--self-contained",
+            "-p:DebugSymbols=false",
             "-p:PublishSingleFile=true",
-            "-p:IncludeNativeLibrariesForSelfExtract=true",
+            "-p:PublishReadyToRun=true",
             "-o", buildpath/"publish"
 
     bin.install buildpath/"publish/test-sharp"
@@ -46,8 +30,26 @@ class TestSharp < Formula
 
   test do
     # Update to whatever your app prints/does
-    output = shell_output("#{bin}/test-sharp").strip
-    assert_match "Hello, World!", output
+    # output = shell_output("#{bin}/test-sharp").strip
+    # assert_match "Hello, World!", output
     # assert_match "Hello, World!", "Hello, World!"
   end
+
+  private
+
+  def rid
+    dotnet_info = Utils.safe_popen_read("dotnet", "--info")
+
+    rid_line = dotnet_info.lines.find do |l|
+      l.start_with?(" RID:", "RID:")
+    end
+
+    odie "Could not determine .NET RID from `dotnet --info`" if rid_line.nil?
+
+    id = rid_line.split(":", 2).last&.strip
+    odie "Could not parse RID from `dotnet --info`" if rid.nil? || rid.empty?
+
+    id
+  end
+
 end
